baf514faea614d7138b07e572823f86c216f785a
Anna Topalidi <t.opalidisanna@gmail.com>
2025-09-09 15:36:16 +0200

    update the decidim branch to "develop"

--- lib/decidim/action_delegator/version.rb
+++ lib/decidim/action_delegator/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-action_delegator version.
   module ActionDelegator
     VERSION = "0.9.0"
-    DECIDIM_VERSION = { github: "decidim/decidim", branch: "feature/add-election-voting-booth" }.freeze
+    DECIDIM_VERSION = { github: "decidim/decidim", branch: "develop" }.freeze
     COMPAT_DECIDIM_VERSION = [">= 0.31.0.dev", "< 0.32"].freeze
   end
 end
