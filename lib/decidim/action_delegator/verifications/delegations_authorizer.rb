d762c953a3d979569ee2b529f75d052a6e538b90
Ivan Vergés <ivan@pokecode.net>
2025-10-03 13:43:50 +0200

    use action authorizer

--- lib/decidim/action_delegator/verifications/delegations_authorizer.rb
+++ lib/decidim/action_delegator/verifications/delegations_authorizer.rb
@@ -5,68 +5,110 @@ module Decidim
     module Verifications
       class DelegationsAuthorizer < Decidim::Verifications::DefaultActionAuthorizer
         def authorize
-          status = super
-
-          if component && component.manifest_name == "elections"
-            return [:ok, {}] if user_in_election_census?
-
+          if !authorization
+            [:missing, { action: :authorize }]
+          elsif authorization_expired?
+            [:expired, { action: :authorize }]
+          elsif !authorization.granted?
+            [:pending, { action: :resume }]
+          elsif invalid_setting?
             return [:unauthorized, { extra_explanation: extra_explanations }]
+          else
+            [:ok, {}]
           end
+        end
 
-          status
+        def invalid_setting?
+          return true unless setting
+          return true unless user_is_a_participant?
+
+          false
         end
 
         private
 
-        def user_in_election_census?
-          setting_id = options["setting"]
-          return false unless setting_id
+        def user_is_a_participant?
+          return false unless setting && setting.active?
 
-          setting = Decidim::ActionDelegator::Setting.find_by(id: setting_id)
-          return false unless setting
-
-          setting.participants.where(decidim_user: authorization.user)
-                 .or(setting.participants.where(census_params(setting)))
-                 .exists?
+          setting.participants.exists?(participant_params(setting))
         end
 
-        def census_params(setting)
-          params = { email: authorization.user.email }
+        def participant_params(setting)
+          params = {}
+          params[:email] = authorization.user.email if setting.email_required?
           params[:phone] = authorization.metadata["phone"] if setting.phone_required?
           params
+        end
+
+        def authorizations_path
+          Decidim::Verifications::Engine.routes.url_helpers.authorizations_path
         end
 
         def extra_explanations
           return @extra_explanations if @extra_explanations
 
-          unless setting
-            return [{
-              key: "no_setting",
-              params: { scope: "decidim.action_delegator.delegations_authorizer" }
-            }]
-          end
+          @extra_explanations = []
 
-          @extra_explanations = [{
-            key: "not_in_census",
-            params: { scope: "decidim.action_delegator.delegations_authorizer" }
-          }]
-
-          @extra_explanations << {
-            key: "email",
-            params: { scope: "decidim.action_delegator.delegations_authorizer", email: authorization.user.email }
-          }
-
-          if setting.phone_required?
+          if setting
+            if setting.active?
+              @extra_explanations << {
+                key: "not_in_census_html",
+                params: {
+                  scope: "decidim.action_delegator.delegations_authorizer",
+                  census: translated_attribute(setting.title),
+                  renew_path: authorizations_path
+                }
+              }
+              if setting.email_required?
+                @extra_explanations << {
+                  key: "email",
+                  params: { scope: "decidim.action_delegator.delegations_authorizer", email: authorization.user.email }
+                }
+              end
+              if setting.phone_required?
+                @extra_explanations << {
+                  key: "phone",
+                  params: { scope: "decidim.action_delegator.delegations_authorizer", phone: authorization.metadata["phone"] || "---" }
+                }
+              end
+            else
+              @extra_explanations << {
+                key: "inactive_setting_html",
+                params: {
+                  scope: "decidim.action_delegator.delegations_authorizer",
+                  census: translated_attribute(setting.title),
+                  renew_path: authorizations_path
+                }
+              }
+            end
+          else
             @extra_explanations << {
-              key: "phone",
-              params: { scope: "decidim.action_delegator.delegations_authorizer", phone: authorization.metadata["phone"] || "---" }
+              key: "no_setting_html",
+              params: {
+                scope: "decidim.action_delegator.delegations_authorizer",
+                renew_path: authorizations_path
+              }
             }
           end
+
           @extra_explanations
         end
 
+        # Obtain the setting from the resource (in case is an election) or from the standard options (coming from the component/resource permissions hash) otherwise
+        def setting_id
+          @setting_id ||= if resource && resource.respond_to?(:census_settings) && resource.respond_to?(:census_manifest)
+                            if resource.census_manifest == "action_delegator_census"
+                              resource.census_settings["setting_id"]
+                            else
+                              resource.census_settings.dig("authorization_handlers", "delegations_verifier", "options", "setting_id")
+                            end
+                          else
+                            options["setting"]
+                          end
+        end
+
         def setting
-          @setting ||= Decidim::ActionDelegator::Setting.find_by(id: authorization.metadata["setting_id"])
+          @setting ||= Decidim::ActionDelegator::Setting.find_by(id: setting_id)
         end
       end
     end
